4288dfe76d50cc4e47adaead07385c46be8666d6
drewcurran <denisac@princeton.edu>
2022-10-19 00:29:17 -0400

    module complete

--- db_edit.py
+++ db_edit.py
@@ -10,168 +10,171 @@ import sys
 import psycopg2
 from psycopg2 import sql
 from psycopg2.extensions import AsIs
-import db_table
 
 # -----------------------------------------------------------------------
 
-def execute(cursor, stmt_str, p=[], l=[]):
-    stmt = sql.SQL(stmt_str).format(*p)
-    print(stmt.as_string(cursor.connection) % tuple(lit.getquoted()
-    for lit in l))
-    cursor.execute(stmt, l)
+class Table:
 
-def drop(cursor, table):
-    execute(cursor, 'BEGIN')
+    def __init__(self, name, index, vals, cursor):
+        self._name = name
+        self._index = index
+        self._vals = vals
+        self._attributes = {**index, **vals}
+        self._cursor = cursor
+        self._transacting = False
 
-    parameters = []
-    name = table.get_name()
-    parameters.append(sql.Identifier(name))
+    def get_name(self):
+        return self._name
 
-    stmt_str = "DROP TABLE IF EXISTS {}"
-
-    execute(cursor, stmt_str, p=parameters)
-
-    execute(cursor, 'COMMIT')
-
-def create(cursor, table):
-    execute(cursor, 'BEGIN')
-
-    parameters = []
-    literals = []
-    name = table.get_name()
-    parameters.append(sql.Identifier(name))    
-    attributes = table.get_attributes()
-    for key, val in attributes.items():
-        parameters.append(sql.Identifier(key))
-        literals.append(AsIs(val))
+    def get_index(self):
+        return self._index
     
-    stmt_str = "CREATE TABLE {}"
-    for i in range(len(attributes)):
-        if i == 0:
-            stmt_str += " ({} %s"
-        else:
-            stmt_str += ", {} %s"
-        if i == len(attributes) - 1:
-            stmt_str += ")"
+    def get_vals(self):
+        return self._vals
+
+    def get_attributes(self):
+        return self._attributes
     
-    execute(cursor, stmt_str, p=parameters, l=literals)
+    def get_transacting(self):
+        return self._transacting
 
-    execute(cursor, 'COMMIT')
-
-def selectAll(cursor, table):
-    # execute(cursor, 'BEGIN')
-
-    parameters = []
-    name = table.get_name()
-    parameters.append(sql.Identifier(name))
-
-    stmt_str = "SELECT * FROM {}"
-
-    execute(cursor, stmt_str, parameters)
-
-    # execute(cursor, 'COMMIT')
-
-def display(cursor, table):
-    execute(cursor, 'BEGIN')
+    def execute(self, stmt_str, p=[], l=[]):
+        stmt = sql.SQL(stmt_str).format(*p)
+        self._cursor.execute(stmt, l)
+        return stmt.as_string(self._cursor.connection) % tuple(lit.getquoted()
+        for lit in l)
     
-    selectAll(cursor, table)
-
-    name = table.get_name()
-    print(('-'*43 + '\n%s\n' + '-'*43) % name)
-
-    row = cursor.fetchone()
-    while row is not None:
-        print(row)
-        row = cursor.fetchone()
-
-    execute(cursor, 'COMMIT')
+    def begin(self):
+        if not self._transacting:
+            return self.execute("BEGIN")
+        return None
     
-def update(cursor, table, index, vals):
-    execute(cursor, 'BEGIN')
-
-    parameters = []
-    name = table.get_name()
-    parameters.append(sql.Identifier(name))
-    for key, val in vals.items():
-        parameters.append(sql.Identifier(key))
-        parameters.append(sql.Literal(val))
-    for key, val in index.items():
-        parameters.append(sql.Identifier(key))
-        parameters.append(sql.Literal(val))
+    def commit(self):
+        if self._transacting:
+            return self.execute("COMMIT")
+        return None
     
-    stmt_str = "UPDATE {} SET"
-    for i in range(len(vals)):
-        if i == 0:
-            stmt_str += " {} = NULLIF({}, NULL)"
-        else:
-            stmt_str += ", {} = NULLIF({}, NULL)"
-    stmt_str += " WHERE"
-    for i in range(len(index)):
-        if i == 0:
-            stmt_str += " {} = {}"
-        else:
-            stmt_str += "AND {} = {}"
+    def commit(self):
+        if self._transacting:
+            return self.execute("ROLLBACK")
+        return None
+        
+    def drop(self):
+        parameters = []
+        parameters.append(sql.Identifier(self._name))
 
-    execute(cursor, stmt_str, p=parameters)
+        stmt_str = "DROP TABLE IF EXISTS {}"
 
-    execute(cursor, 'COMMIT')
+        self.execute(stmt_str, p=parameters)
 
+    def create(self):
+        parameters = []
+        literals = []
+        parameters.append(sql.Identifier(self._name))
+        for key, val in self._attributes.items():
+            parameters.append(sql.Identifier(key))
+            literals.append(AsIs(val))
+        
+        stmt_str = "CREATE TABLE {}"
+        for i in range(len(self._attributes)):
+            if i == 0:
+                stmt_str += " ({} %s"
+            else:
+                stmt_str += ", {} %s"
+            if i == len(self._attributes) - 1:
+                stmt_str += ")"
+        
+        self.execute(stmt_str, p=parameters, l=literals)
 
-def insert(cursor, table, vals):
-    execute(cursor, 'BEGIN')
+    def selectAll(self):
+        parameters = []
+        parameters.append(sql.Identifier(self._name))
 
-    parameters = []
-    name = table.get_name()
-    parameters.append(sql.Identifier(name))
-    parameters += list(map(lambda k : sql.Identifier(str(k)), vals.keys()))
-    parameters += list(map(lambda v : sql.Literal(str(v)), vals.values()))
+        stmt_str = "SELECT * FROM {}"
 
-    stmt_str = "INSERT INTO {}"
-    for i in range(len(vals)):
-        if i == 0:
-            stmt_str += " ({}"
-        else:
-            stmt_str += ", {}"
-        if i == len(vals) - 1:
-            stmt_str += ")"
-    stmt_str += " VALUES"
-    for i in range(len(vals)):
-        if i == 0:
-            stmt_str += " ({}"
-        else:
-            stmt_str += ", {}"
-        if i == len(vals) - 1:
-            stmt_str += ")"
-    
-    execute(cursor, stmt_str, p=parameters)
+        self.execute(stmt_str, parameters)
 
-    execute(cursor, 'COMMIT')
-    
-def delete(cursor, table, index):
-    name = table.get_name()
-    keys = table.get_attributes().keys()
-    index_vals = []
-    for key in keys():
-        try:
-            index_val = format_val(index[key])
-            index_vals.append(key, index_val)
-        except:
-            pass
-    parameters = tuple(index_vals)
-    stmt_str = "DELETE FROM " + name
-    for i in range(len(index_vals)):
-        if i == 0:
-            stmt_str += " WHERE %s = %s"
-        else:
-            stmt_str += " AND %s = %s"
-    cursor.execute(stmt_str, parameters)
+    def displayAll(self):
+        self.selectAll()
+        print(('-'*43 + '\n%s\n' + '-'*43) % self._name)
 
-    cursor.execute('COMMIT')
+        row = self._cursor.fetchone()
+        while row is not None:
+            print(row)
+            row = self._cursor.fetchone()
+        
+    def update(self, index, vals):
+        parameters = []
+        literals = []
+        parameters.append(sql.Identifier(self._name))
+        for key, val in vals.items():
+            parameters.append(sql.Identifier(key))
+            parameters.append(sql.Literal(val))
+            literals.append(AsIs('B') if self._attributes[key] == 'BIT(2)' else AsIs(''))
+        for key, val in index.items():
+            parameters.append(sql.Identifier(key))
+            parameters.append(sql.Literal(val))
+            literals.append(AsIs('B') if self._attributes[key] == 'BIT(2)' else AsIs(''))
+        
+        stmt_str = "UPDATE {} SET"
+        for i in range(len(vals)):
+            if i == 0:
+                stmt_str += " {} = NULLIF(%s{}, NULL)"
+            else:
+                stmt_str += ", {} = NULLIF(%s{}, NULL)"
+        stmt_str += " WHERE"
+        for i in range(len(index)):
+            if i == 0:
+                stmt_str += " {} = %s{}"
+            else:
+                stmt_str += "AND {} = %s{}"
+
+        self.execute(stmt_str, p=parameters, l=literals)
+
+    def insert(self, vals):
+        parameters = []
+        parameters.append(sql.Identifier(self._name))
+        parameters += list(map(lambda k : sql.Identifier(str(k)), vals.keys()))
+        parameters += list(map(lambda v : sql.Literal(str(v)), vals.values()))
+
+        stmt_str = "INSERT INTO {}"
+        for i in range(len(vals)):
+            if i == 0:
+                stmt_str += " ({}"
+            else:
+                stmt_str += ", {}"
+            if i == len(vals) - 1:
+                stmt_str += ")"
+        stmt_str += " VALUES"
+        for i in range(len(vals)):
+            if i == 0:
+                stmt_str += " ({}"
+            else:
+                stmt_str += ", {}"
+            if i == len(vals) - 1:
+                stmt_str += ")"
+        
+        self.execute(stmt_str, p=parameters)
+        
+    def delete(self, index):
+        parameters = []
+        literals = []
+        parameters.append(sql.Identifier(self._name))
+        for key, val in index.items():
+            parameters.append(sql.Identifier(key))
+            parameters.append(sql.Literal(val))
+            literals.append(AsIs('B') if self._attributes[key] == 'BIT(2)' else AsIs(''))
+        
+        stmt_str = "DELETE FROM {} WHERE"
+        for i in range(len(index)):
+            if i == 0:
+                stmt_str += " {} = %s{}"
+            else:
+                stmt_str += "AND {} = %s{}"
+
+        self.execute(stmt_str, p=parameters, l=literals)
 
 def main():
-    clubs = db_table.Table('clubs', {'clubid':'INTEGER'}, 
-    {'name':'TEXT', 'description':'TEXT', 'info_shared':'BIT(2)'})
-
     if len(sys.argv) != 2:
         print('usage: python %s db_url' % sys.argv[0],
             file=sys.stderr)
@@ -184,23 +187,25 @@ def main():
         db_url = os.getenv('ELEPHANTSQL_URL')
         with psycopg2.connect(db_url) as connection:
             with connection.cursor() as cursor:
-                drop(cursor, clubs)
-                create(cursor, clubs)
-                insert(cursor, clubs, {'clubid':1, 
+                clubs = Table('clubs', {'clubid':'INTEGER'}, 
+                {'name':'TEXT', 'description':'TEXT', 'info_shared':'BIT(2)'},
+                cursor)
+
+                clubs.drop()
+                clubs.create()
+                clubs.insert({'clubid':1, 
                 'name':'Women\'s Club Lacrosse',
                 'description':'Free for all to join!', 
                 'info_shared':'11'})
-                display(cursor, clubs)
-                update(cursor, clubs,
-                {'name':'Women\'s Club Lacrosse'}, {'clubid':8,
-                'info_shared':'00'
-                })
-                display(cursor, clubs)
+                clubs.displayAll()
 
-                # display(cursor, clubs)
-                # delete(cursor, clubs, {'clubid':'1', 
-                # 'name':'Women\'s Club Lacrosse'})
-                # display(cursor, clubs)
+                clubs.update({'name':'Women\'s Club Lacrosse'},
+                {'clubid':8, 'info_shared':'00'})
+                clubs.displayAll()
+
+                clubs.delete({'clubid':8, 
+                'name':'Women\'s Club Lacrosse'})
+                clubs.displayAll()
 
     except Exception as ex:
         print(ex, file=sys.stderr)
