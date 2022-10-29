b574114f7d5a21462c0120efabeff49f49d47336
Richard Cheng <rc38@princeton.edu>
2022-10-29 07:27:03 -0400

    three pages done

---
+++ src/index.js
@@ -0,0 +1,22 @@
+console.log("hi")
+
+function toggleMenu() {
+  let filter = document.getElementById("filter")
+  let menu = document.getElementById("menu")
+  filter.classList.toggle("hidden")
+  menu.classList.toggle("hidden")
+}
+
+function profileNext(currId, nextId) {
+  if (!nextId) {
+    window.location="index.html"
+    return
+  }
+  let currForm = document.getElementById(currId)
+  let nextForm = document.getElementById(nextId)
+  console.log(currForm, nextForm)
+  currForm.classList.toggle("hidden")
+  nextForm.classList.toggle("hidden")
+  return
+}
+
--- static_files/index.js
+++
@@ -1,9 +0,0 @@
-console.log("hi")
-
-function toggleMenu() {
-  let filter = document.getElementById("filter")
-  let menu = document.getElementById("menu")
-  filter.classList.toggle("hidden")
-  menu.classList.toggle("hidden")
-}
-
