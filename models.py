d3000632284196c3c7dd4bd442b620dbdc3cdd11
drewcurran <denisac@princeton.edu>
2022-10-29 16:03:00 -0400

    Update models.py

--- models.py
+++ models.py
@@ -41,6 +41,11 @@ class ClubsModel(db.Model):
     clubid = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     description = db.Column(db.String)
+    # Possible to make info_shared a collection of boolean columns?
+    # Would help for modularity and readability
+    # Could keep it as a string - would need to encode/decode
+    # Could have a boolean var for each attribute also
+    # More generally, store information everywhere as a list or as separate vars?
     info_shared = db.Column(db.String)
 
     def __init__(self, clubid, name, description, info_shared):
@@ -55,7 +60,7 @@ class ClubMembersModel(db.Model):
     __tablename__ = "clubmembers"
 
     netid = db.Column(db.String, primary_key=True)
-    clubid = db.Column(db.Integer)
+    clubid = db.Column(db.Integer, primary_key=True)
     is_moderator = db.Column(db.Boolean)
 
     def __init__(self, clubid, netid, is_moderator):
@@ -65,7 +70,31 @@ class ClubMembersModel(db.Model):
 
 
 ## Model for club join requests in database
-##class JoinRequests(db.Model)
+class JoinRequests(db.Model):
+    __tablename__ = "joinreqs"
+
+    joinid = db.Column(db.Integer, primary_key=True)
+    clubid = db.Column(db.Integer)
+    netid = db.Column(db.Integer)
+
+    def __init__(self, joinid, clubid, netid):
+        self.joinid = joinid
+        self.clubid = clubid
+        self.netid = netid
 
 ## Model for club creation requests in database
-##class CreationRequests(db.Model)
+class CreationRequests(db.Model):
+    __tablename__ = "creationreqs"
+
+    createid = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    netid = db.Column(db.Integer)
+    description = db.Column(db.String)
+    info_shared = db.Column(db.String)
+
+    def __init__(self, createid, name, netid, description, info_shared):
+        self.createid = createid
+        self.name = name
+        self.netid = netid
+        self.description = description
+        self.info_shared = info_shared
