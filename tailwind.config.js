6de33c9f305384029a0be71908492c26069a430b
Evan Wang <evanwang@princeton.edu>
2022-11-06 17:19:15 -0500

    helvetica

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.{html,js,css}"],
+  content: ["./static_files/**/*.{html,js}",
+"./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
