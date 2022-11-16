b090981ec3433f98c3ea17d2df37f610ef19fa8f
Daniel Hu <danielhu@dynamic-oit-vapornet100-10-9-108-35.princeton.edu>
2022-11-15 23:20:47 -0500

    add photo links

--- query.py
+++ query.py
@@ -1,8 +1,12 @@
 import sqlalchemy
 
 engine = sqlalchemy.create_engine("postgresql+psycopg2://stwiezab:eN4T8unVzyIE49TzhKCbf1m5lKkGhjWU@peanut.db.elephantsql.com/stwiezab")
+metadata = sqlalchemy.MetaData(bind=engine)
+sqlalchemy.MetaData.reflect(metadata)
+
+users_table = metadata.tables['users']
 
 with engine.connect() as connection:
   result = connection.execute(sqlalchemy.text("SELECT * FROM allundergrads"))
   for row in result:
-    print(row[0])
+    connection.execute(sqlalchemy.insert(users_table).values(netid=row[0], photo=row[1]))
