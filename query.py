3bdd6157ca855cc9552a5eaaad0daed0dc63b396
Daniel Hu <danielhu@dynamic-oit-vapornet100-10-9-108-35.princeton.edu>
2022-11-20 22:12:24 -0500

    crawling code

--- query.py
+++ query.py
@@ -8,5 +8,10 @@ users_table = metadata.tables['users']
 
 with engine.connect() as connection:
   result = connection.execute(sqlalchemy.text("SELECT * FROM allundergrads"))
+
+
   for row in result:
-    connection.execute(sqlalchemy.insert(users_table).values(netid=row[0], photo=row[1]))
+    stmt = "SELECT users.photo FROM users WHERE users.netid=" + row[0]
+    has_photo = connection.execute(sqlalchemy.text(stmt))
+    if has_photo == None:
+      connection.execute(sqlalchemy.insert(users_table).values(netid=row[0], photo=None))
