f163f4c0ca4509076988852400d5815b006a139b
Richard Cheng <rc38@princeton.edu>
2022-12-02 11:38:02 -0500

    user info page revised

--- static_files/index.js
+++ static_files/index.js
@@ -4,7 +4,7 @@ function toggleMenu() {
   let filter = document.getElementById("filter")
   let menu = document.getElementById("menu")
   filter.classList.toggle("hidden")
-  menu.classList.toggle("hidden")
+  menu.classList.toggle("reveal")
 }
 
 function cloudsMove() {
