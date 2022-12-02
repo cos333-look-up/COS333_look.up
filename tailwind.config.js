667fee408e880c6c3ac952bb8f702fa0c9d4a132
Evan Wang <evanwang@princeton.edu>
2022-12-02 02:59:12 -0500

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./static_files/**/*.{html,js}",
-"./src/**/*.{html,js}"],
+    "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'offwhite': '#fffaee'
+      },
+    },
+
   },
   plugins: [],
 }
