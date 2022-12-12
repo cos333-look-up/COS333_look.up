950d484e92d0612214648e847ca2054133b2a594
Daniel Hu <dh37@princeton.edu>
2022-12-12 14:11:43 -0500

    removed checks for cloudinary being in the person's photo: it breaks if users don't have a photo.

--- query.py
+++ query.py
@@ -1,4 +1,6 @@
 import sqlalchemy
+from api import req_lib
+import json
 
 engine = sqlalchemy.create_engine("postgresql+psycopg2://stwiezab:eN4T8unVzyIE49TzhKCbf1m5lKkGhjWU@peanut.db.elephantsql.com/stwiezab")
 metadata = sqlalchemy.MetaData(bind=engine)
@@ -7,11 +9,21 @@ sqlalchemy.MetaData.reflect(metadata)
 users_table = metadata.tables['users']
 
 with engine.connect() as connection:
-  result = connection.execute(sqlalchemy.text("SELECT * FROM allundergrads"))
-
+  req = req_lib.ReqLib()
+  stmt = "SELECT * FROM users WHERE users.display_name IS NULL"
+  result = connection.execute(sqlalchemy.text(stmt))
 
   for row in result:
-    stmt = "SELECT users.photo FROM users WHERE users.netid=" + row[0]
-    has_photo = connection.execute(sqlalchemy.text(stmt))
-    if has_photo == None:
-      connection.execute(sqlalchemy.insert(users_table).values(netid=row[0], photo=None))
+    netid = row[0]
+    result = req.getJSON(req.configs.USERS_FULL, uid=netid)
+    if result:
+      display_name = result[0]['displayname']
+      print(display_name)
+      connection.execute(sqlalchemy.update(users_table)
+        .where(users_table.c.netid == netid)
+        .values(
+            is_admin=False,
+            is_banned=False,
+            display_name=display_name
+        ))
+  print('All done!')
