fa7e195774154a59d8f853b7550d7b389efd52f6
Richard Cheng <rc38@princeton.edu>
2022-12-12 07:22:11 -0500

    new group create form

--- static_files/index.js
+++ static_files/index.js
@@ -31,15 +31,15 @@ function cloudsMove() {
 
 }
 
-function profileNext(currId, nextId) {
-  if (!nextId) {
-    window.location = "index"
-    return
-  }
-  let currForm = document.getElementById(currId)
-  let nextForm = document.getElementById(nextId)
-  console.log(currForm, nextForm)
-  currForm.classList.toggle("hidden")
-  nextForm.classList.toggle("hidden")
-  return
-}
+// function profileNext(currId, nextId) {
+//   if (!nextId) {
+//     window.location = "index"
+//     return
+//   }
+//   let currForm = document.getElementById(currId)
+//   let nextForm = document.getElementById(nextId)
+//   console.log(currForm, nextForm)
+//   currForm.classList.toggle("hidden")
+//   nextForm.classList.toggle("hidden")
+//   return
+// }
